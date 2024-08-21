8efad3b47ae65d4ad9de9170119ff31c5daa7061
jmuller <jmuller@quintor.nl>
2024-08-21 08:58:41 +0200

    added some extra information for swagger

--- Backend/src/main/java/com/jamgm/CalTracker/web/rest/FoodItemController.java
+++ Backend/src/main/java/com/jamgm/CalTracker/web/rest/FoodItemController.java
@@ -2,6 +2,7 @@ package com.jamgm.CalTracker.web.rest;
 
 import com.jamgm.CalTracker.model.FoodItem;
 import com.jamgm.CalTracker.service.FoodItemService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class FoodItemController {
     }
 
     @GetMapping(value = "/{barcode}")
+    @Operation(summary = "Get nutrition information by barcode")
     public Mono<FoodItem> getFoodItemByBarcode(@PathVariable("barcode") final long barcode){
         return this.foodItemService.getFoodItemByBarcode(barcode);
     }
