6b7c281b5deb5374502b216c1a28dd33314f1522
jmuller <jmuller@quintor.nl>
2024-08-27 13:42:50 +0200

    added encryption for user passwords

--- Backend/src/main/java/com/jamgm/CalTracker/service/UserService.java
+++ Backend/src/main/java/com/jamgm/CalTracker/service/UserService.java
@@ -2,19 +2,23 @@ package com.jamgm.CalTracker.service;
 
 import com.jamgm.CalTracker.model.User;
 import com.jamgm.CalTracker.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserService {
     private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
     public User createUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
