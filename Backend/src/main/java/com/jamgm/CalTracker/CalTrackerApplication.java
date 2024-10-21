f5a06eaafbca81b66eb38924ae018d91fdb48673
jmuller <jmuller@quintor.nl>
2024-10-21 09:12:27 +0200

    Got rid of some commented code

--- Backend/src/main/java/com/jamgm/CalTracker/CalTrackerApplication.java
+++ Backend/src/main/java/com/jamgm/CalTracker/CalTrackerApplication.java
@@ -9,7 +9,4 @@ public class CalTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalTrackerApplication.class, args);
 	}
-
-	//https://openfoodfacts.github.io/openfoodfacts-server/api/ref-v2/#get-/api/v2/search
-	//https://openfoodfacts.github.io/openfoodfacts-server/api/
 }
