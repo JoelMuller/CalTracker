18951f0be1f3f9f104f5a350596380a80ceff96b
jmuller <jmuller@quintor.nl>
2024-10-23 11:11:37 +0200

    added in memory database configuration for tests

--- Backend/src/test/java/com/jamgm/CalTracker/config/TestApplicationContext.java
+++ Backend/src/test/java/com/jamgm/CalTracker/config/TestApplicationContext.java
@@ -2,8 +2,11 @@ package com.jamgm.CalTracker.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @TestConfiguration
 @ComponentScan(basePackages = "com.jamgm.CalTracker")
+@PropertySource("application-test.properties")
 public class TestApplicationContext {
 }
